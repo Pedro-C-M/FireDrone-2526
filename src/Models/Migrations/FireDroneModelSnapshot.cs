8c983d3a640ced178a8b698d647148cf00b5570a
UO287758 <UO287758@uniovi.es>
2025-11-10 16:10:19 +0100

    New try

--- src/Models/Migrations/FireDroneModelSnapshot.cs
+++ src/Models/Migrations/FireDroneModelSnapshot.cs
@@ -14,7 +14,7 @@ namespace Models.Migrations
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
+            modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
 
             modelBuilder.Entity("BaseStation", b =>
                 {
