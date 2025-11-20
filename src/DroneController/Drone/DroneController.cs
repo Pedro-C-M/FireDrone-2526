ad7867da865c6d443c0834ad1f61edc2ba38fc55
uo288120 <145131850+Pedro-C-M@users.noreply.github.com>
2025-11-20 20:09:58 +0100

    Drone loggea los comandos

--- src/DroneController/Drone/DroneController.cs
+++ src/DroneController/Drone/DroneController.cs
@@ -100,9 +100,8 @@ namespace DroneController.Drone
                 var message = Encoding.UTF8.GetString(ea.Body.ToArray());
                 var routingKey = ea.RoutingKey;
 
-                // Consuming example: just log the received message.
-                // In the real application, you process the message as needed.
-                Debug.WriteLine($"[x] Received '{routingKey}':'{message}'");
+                HandleDroneCommand(message);
+
                 return Task.CompletedTask;
             };
 
@@ -140,15 +139,27 @@ namespace DroneController.Drone
 
 			if (command.Command == DroneCommand.START_FLIGHT_PLAN_CMD)
 			{
-				// Decodificar los argumentos
-				Waypoint[] waypoints = JsonConvert.DeserializeObject<Waypoint[]>(command.Arguments);
-				_drone.StartFlightPlan(waypoints);
+                // Decodificar los argumentos
+                //Waypoint[] waypoints = JsonConvert.DeserializeObject<Waypoint[]>(command.Arguments);
+                Waypoint[] waypoints = //De ejemplo rellenado
+				{
+					new Waypoint { Latitude = 43.36, Longitude = -5.84, Altitude = 50, Speed = 20 },
+					new Waypoint { Latitude = 43.361, Longitude = -5.841, Altitude = 55, Speed = 22 },
+					new Waypoint { Latitude = 43.362, Longitude = -5.842, Altitude = 60, Speed = 25 },
+					new Waypoint { Latitude = 43.363, Longitude = -5.843, Altitude = 65, Speed = 20 },
+					new Waypoint { Latitude = 43.364, Longitude = -5.844, Altitude = 70, Speed = 18 }
+				};
+               // _drone.StartFlightPlan(waypoints);
 			}
 			else if (command.Command == DroneCommand.STOP_FLIGHT_PLAN_CMD)
 			{
-				_drone.StopFlightPlan();
+                //_drone.StopFlightPlan();
 			}
-			else if (command.Command == DroneCommand.STATUS_CMD)
+            else if (command.Command == DroneCommand.GOTO_MANUAL)
+            {
+				Console.WriteLine("GOTO_MANUAL");
+            }
+            else if (command.Command == DroneCommand.GOTO_MANUAL)//Este para el status
 			{
 				DroneStatus status = _drone.GetStatus();
 
