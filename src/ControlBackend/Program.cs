260cfd8ac428af10c44fdf64c07ed49ab2935ef2
uo288120 <145131850+Pedro-C-M@users.noreply.github.com>
2025-11-22 15:50:12 +0100

    Limpio program de backend controller

--- src/ControlBackend/Program.cs
+++ src/ControlBackend/Program.cs
@@ -2,35 +2,6 @@
 namespace ControlBackend;
 
 public class Program
-
-/**
- * PARA CUANDO SE HAGAN LOS ENDPOINTS Y LA API COMO SE USA RABBITMQ
- * 
- * [ApiController]
-    [Route("[controller]")]
-    public class DroneController : ControllerBase
-    {
-    private readonly IPublisher _publisher;
-
-    public DroneController(IPublisher publisher)
-    {
-      _publisher = publisher;
-    }
-
-    [HttpGet("sendtest")]
-    public async Task<IActionResult> SendTestMessage()
-    {
-      var message = "Hola desde backend: " + DateTime.Now;
-      var body = Encoding.UTF8.GetBytes(message);
-
-      await _publisher.PublishAsync("drone.test", body);
-
-      return Ok("Mensaje enviado!");
-    }
-    }
-    Así, cualquier endpoint de los controlles puede enviar mensajes a los drones sin necesidad de crear conexión o canal cada vez.
- * 
- */
 {
     public static void Main(string[] args)
     {
@@ -63,31 +34,8 @@ public class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-
         app.UseAuthorization();
-
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         app.MapControllers();
-
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-        {
-            var forecast =  Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                {
-                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    TemperatureC = Random.Shared.Next(-20, 55),
-                    Summary = summaries[Random.Shared.Next(summaries.Length)]
-                })
-                .ToArray();
-            return forecast;
-        })
-        .WithName("GetWeatherForecast")
-        .WithOpenApi();
-
         app.Run();
     }
 }
