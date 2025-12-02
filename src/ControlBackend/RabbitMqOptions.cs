ba61023c4abf27b2388d83af3f51e85ecdcad1f6
PedroCM <145131850+Pedro-C-M@users.noreply.github.com>
2025-12-02 17:55:26 +0100

    Cambio RabbitMQ de local a la MV

--- src/ControlBackend/RabbitMqOptions.cs
+++ src/ControlBackend/RabbitMqOptions.cs
@@ -7,10 +7,13 @@
      * 
      * Esto facilita desplegar el sistema en distintos entornos (local, servidor, nube)
      * sin necesidad de modificar el código fuente usando variables de entorno.
+     *
+     * http://156.35.163.122:15672/ - RabbitMQ Management UI de la MV contraseña y usuario son admin admin
      */
     public class RabbitMqOptions
     {
-        public string Hostname { get; set; } = Environment.GetEnvironmentVariable("RABBITMQ_HOST") ?? "localhost";
+        //public string Hostname { get; set; } = Environment.GetEnvironmentVariable("RABBITMQ_HOST") ?? "localhost";
+        public string Hostname { get; set; } = Environment.GetEnvironmentVariable("RABBITMQ_HOST") ?? "156.35.163.122";//IP de la maquina virtual
         public string Username { get; set; } = Environment.GetEnvironmentVariable("RABBITMQ_USER") ?? "guest";
         public string Password { get; set; } = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD") ?? "guest";
         public string Exchange { get; set; } = Environment.GetEnvironmentVariable("RABBITMQ_EXCHANGE") ?? "drone_exchange";
