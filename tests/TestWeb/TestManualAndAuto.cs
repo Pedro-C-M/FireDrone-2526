9cfac1395c09a9f6ebc32d839eafae7047e239dd
uo288120 <145131850+Pedro-C-M@users.noreply.github.com>
2026-03-06 13:50:14 +0100

    Ignoro test web para tardar poco en integracion continua RECORDAR REACTIVAR

--- tests/TestWeb/TestManualAndAuto.cs
+++ tests/TestWeb/TestManualAndAuto.cs
@@ -6,6 +6,7 @@ using System.Threading;
 
 namespace TestWeb
 {
+    [Ignore("Desactivado temporalmente para acelerar la Integración Continua")]
     [TestClass()]
     public class TestManualAndAuto : WebTestBase
     {
